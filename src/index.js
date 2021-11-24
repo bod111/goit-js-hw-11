c20fa76401d38af02479cf5de06e9d09a2a1c4aa
bod111 <86015208+bod111@users.noreply.github.com>
2021-11-24 08:46:45 +0200

    third

--- src/index.js
+++ src/index.js
@@ -7,18 +7,18 @@ import { cardMarkup } from './js/carg-markup.js';
 import { refs } from './js/refs.js';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-let searchItems = '';
+let searchQuery = '';
 let page = 1;
 
 function onFindItems(e) {
   e.preventDefault();
 
-  searchItems = e.currentTarget.elements.searchQuery.value;
-  if (searchItems === '') {
+  searchQuery = e.currentTarget.elements.searchQuery.value;
+  if (searchQuery === '') {
     return;
   }
 
-  fetchGallary(searchItems, page).then(data => {
+  fetchGallary(searchQuery, page).then(data => {
     if (data.totalHits === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.',
--- src/js/api.js
+++ src/js/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const MY_KEY = '23311871-6dfcca62e78c0a60decb58f13';
 
-export default function fetchGallary(searchItems, page) {
+export default function fetchGallary(searchQuery, page) {
   return fetch(
-    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchItems}&key=${MY_KEY}&page=${page}&per_page=40`,
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchQuery}&key=${MY_KEY}&page=${page}&per_page=40`,
   ).then(response => {
     if (response.status === 404) {
       return Promise.reject(new Error(response.message));
